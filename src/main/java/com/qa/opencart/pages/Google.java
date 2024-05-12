62fdd0b583b00e89483b2d74946f4126e80e8ea9
Soni Sridhar <soni.sridhark@gmail.com>
2024-05-12 09:23:16 +0530

    Signed-off-by: Soni Sridhar <soni.sridhark@gmail.com>

--- src/main/java/com/qa/opencart/pages/Google.java
+++ src/main/java/com/qa/opencart/pages/Google.java
@@ -18,6 +18,9 @@ public class Google {
 	public void close() {
 		System.out.println("closing the browser----sonni");
 	}
+	public void getTitle() {
+		System.out.println("get title");
+	}
 
 	int i =10;
 }
