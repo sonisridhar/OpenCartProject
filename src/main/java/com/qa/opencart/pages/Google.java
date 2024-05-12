cf8a1e00f83ee83a1644f0dcfbe11451c8204591
Soni Sridhar <soni.sridhark@gmail.com>
2024-05-12 08:52:34 +0530

    google code

---
+++ src/main/java/com/qa/opencart/pages/Google.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Google {
+	
+	
+	public void login() {
+		System.out.println("Login page---Soni");
+	}
+
+	public void setup() {
+		System.out.println("");
+	}
+}
