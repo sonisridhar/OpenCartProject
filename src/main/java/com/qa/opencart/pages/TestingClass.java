81e05893d4ad53853aeda70e9dac7b2e04180afc
Soni Sridhar <soni.sridhark@gmail.com>
2024-05-12 08:00:06 +0530

    code changes as per comment

--- src/main/java/com/qa/opencart/pages/TestingClass.java
+++ src/main/java/com/qa/opencart/pages/TestingClass.java
@@ -1,10 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class TestingClass {
+	private By mail = By.id("testing");
 
-	public static void main(String[] args) {
-
-		System.out.println("My name is soni");
+	public void getMail() {
+		System.out.println("OpenCart cart page");
 
 	}
 
